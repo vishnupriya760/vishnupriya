6d756843cb8bd34f32cc8c722bbf89e3104d7b96
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:55:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div>
+    <div class="head">
+      <h1 class="v">ellamart+</h1>
+      </div>
+      <div class="head2">
+      
+      <p class="v1">Category</p>
+      <p class="v1"> Theme shop</p>
+      <p class="v1">Shop</p>
+      <p class="v1">Product</p>
+      <p class="v1">Blog</p>
+      <p class="v1">Pages</p>
+      <p class="v1">new In</p>
+      <p class="v1">Trend</p>
+      <p class="v1">Collections</p>
+      </div>
+      
+      
+      
+      
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
